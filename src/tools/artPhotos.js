46f3fbe5e83929e7f84262a6d7b0ae1c4313f07c
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-01-20 21:35:40 -0800

    Create artPhotos.js

---
+++ src/tools/artPhotos.js
@@ -0,0 +1,52 @@
+export const artPhotos = [
+  {
+    img_url: "https://i.imgur.com/BnqdQ1W.jpeg",
+    name: "Miraku",
+    album: "Portraits",
+    date: "10.19.19"
+  },
+  {
+    img_url: "https://i.imgur.com/ItDtKcn.jpeg",
+    name: "Lydia and Desire",
+    album: "Portraits",
+    date: "10.19.19"
+  },
+  {
+    img_url: "https://i.imgur.com/c62ckP5.jpg",
+    name: "Park Portrait",
+    album: "Portraits",
+    date: "10.19.19"
+  },
+  {
+    img_url: "https://i.imgur.com/iSoz2sM.jpg",
+    name: "Watercolor Portrait",
+    album: "Paintings",
+    date: "NA"
+  },
+  {
+    img_url: "https://i.imgur.com/HIiLmhE.jpg",
+    name: "Map Collage",
+    album: "Paintings",
+    date: "NA"
+  },
+
+  {
+    img_url: "https://i.imgur.com/gUI5oAU.jpg",
+    name: "Olokun's Daughter",
+    album: "Paintings",
+    date: "NA"
+  },
+
+  {
+    img_url: "https://i.imgur.com/kEJADqc.jpg",
+    name: "Watercolor Collage",
+    album: "Paintings",
+    date: "NA"
+  },
+  {
+    img_url: "https://i.imgur.com/lXzru44.jpg",
+    name: "Snake Collage",
+    album: "Paintings",
+    date: "NA"
+  }
+];
