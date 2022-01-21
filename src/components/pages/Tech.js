fad991a1001be9a274996e941c4b10e7dbcc579d
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-01-20 21:34:03 -0800

    Create Tech.js

---
+++ src/components/pages/Tech.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "./pages.css";
+import Dropdown from "../menu/Dropdown.js";
+import "../../styles.css";
+
+const Tech = () => {
+  return (
+    <div className="tech">
+      <Link className="name" to="/">
+        Sky Leilani
+      </Link>
+      <div className="button">
+        <Dropdown />
+      </div>
+      <p className="pageTitle">Tech</p>
+    </div>
+  );
+};
+
+export default Tech;
