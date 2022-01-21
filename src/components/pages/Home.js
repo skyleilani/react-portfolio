fe0c7a2a9fc7f620bcc421be746709f7b1b0e3e7
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-01-21 00:43:44 -0800

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,12 @@
-import Carousel from "../carousel/Carousel";
-import Dropdown from "../menu/Dropdown";
-import { homePhotos } from "../../tools/homePhotos";
-import "../../styles.css";
+import Carousel from '../carousel/Carousel';
+import Dropdown from '../menu/Dropdown';
+import {homePhotos} from '../../tools/homePhotos';
+import '../../styles.css';
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="button">
+    <div className='home'>
+      <div className='button'>
         <Dropdown />
       </div>
       <Carousel photos={homePhotos} />
