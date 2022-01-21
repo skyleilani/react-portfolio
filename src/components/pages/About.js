8448ff8eaee618305e666d55df49cef574599315
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-01-21 00:45:15 -0800

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
-import "./pages.css";
-import Dropdown from "../components/menu/Dropdown.js";
-import "../styles.css";
+import {Link} from 'react-router-dom';
+import './pages.css';
+import Dropdown from '../components/menu/Dropdown.js';
+import '../styles.css';
 
 const About = () => {
   return (
-    <div className="about">
-      <Link className="name" to="/">
+    <div className='about'>
+      <Link className='name' to='/'>
         Sky Leilani
       </Link>
-      <div className="button">
+      <div className='button'>
         <Dropdown />
       </div>
-      <p className="pageTitle">About</p>
+      <p className='pageTitle'>About</p>
     </div>
   );
 };
