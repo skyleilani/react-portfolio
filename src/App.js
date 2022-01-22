d69f7a8bfcd973e18bcc0c4b78e27496b95360cc
sky leilani <45158875+skyleilani@users.noreply.github.com>
2022-01-21 16:33:51 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,53 @@ import Home from "./components/pages/Home";
 import Art from "./components/pages/Art";
 import Tech from "./components/pages/Tech";
 import About from "./components/pages/About";
-
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Cursor from "./components/cursor/Cursor";
 const App = () => {
   return (
-    <div className="App">
-      {/*routing*/}
-      <div>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <Home />
-              </div>
-            }
-          />{" "}
-          <Route
-            path="/art"
-            element={
-              <div>
-                <Art />
-              </div>
-            }
-          />{" "}
-          <Route
-            path="/tech"
-            element={
-              <div>
-                <Tech />
-              </div>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <div>
-                <About />
-              </div>
-            }
-          />
-        </Routes>
+    <>
+      <Cursor />
+      <div className="App">
+        {/*routing*/}
+        <div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div>
+                  <Home />
+                </div>
+              }
+            />{" "}
+            <Route
+              path="/art"
+              element={
+                <div>
+                  <Art />
+                </div>
+              }
+            />{" "}
+            <Route
+              path="/tech"
+              element={
+                <div>
+                  <Tech />
+                </div>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <div>
+                  <About />
+                </div>
+              }
+            />
+          </Routes>
+        </div>
+        );
       </div>
-      );
-    </div>
+    </>
   );
 };
 
